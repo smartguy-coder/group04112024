8078dd8e14e60fef8d00dfd010d1f443df30a281
Vasyl <vasyl@example.com>
2024-12-05 20:55:29 +0200

    web start

--- web.py
+++ web.py
@@ -50,6 +50,12 @@ if is_correct_login and is_correct_password:
     name_length = len(given_name)
     if name_length == 1:
         put_success(f'Strange name with length {name_length}')
+    elif name_length > 50:
+        put_success(f'your name contains more 50 symbols {name_length}')
+    elif name_length > 20:
+        put_success(f'your name contains more 20 symbols {name_length}')
+    elif name_length < 5:
+        put_success(f'your name contains less than 5 symbols {name_length}')
     else:
         put_success(f'Name with length {name_length}')
 
