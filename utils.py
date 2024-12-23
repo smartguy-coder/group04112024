6291eb86f68db4f370fdcda602cf95b700967d8c
Vasyl <vasyl@example.com>
2024-12-23 20:29:33 +0200

    more tests

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from constants import ENGLISH_VOWELS
+
+
 def is_even(number: int | float) -> bool:
     print(number)
     print(number % 2)
@@ -25,3 +28,40 @@ def filter_strings_with_n(strings: list[str]) -> list[str]:
         if 'n' in string or 'N' in string:
             result.append(string)
     return result
+
+
+def return_max_discount() -> int:
+    return_value = 100
+    return return_value
+
+
+def get_vowels_from_text(input_string: str = '') -> str:
+    """works only with English"""
+    input_string = input_string.lower()
+
+    result = ''
+    for letter in input_string:
+        if letter in ENGLISH_VOWELS:
+            result += letter
+
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
