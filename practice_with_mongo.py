151f33d34dd7f6db568d740c9efa1b8f655420c7
Vasyl <vasyl@example.com>
2025-02-17 22:01:27 +0200

    mongo crud

--- practice_with_mongo.py
+++ practice_with_mongo.py
@@ -1,6 +1,9 @@
+from pprint import pprint
+
 from config import uri
 
 from pymongo.mongo_client import MongoClient
+from bson import ObjectId
 
 client = MongoClient(uri)
 
@@ -31,5 +34,120 @@ collection_phones = db.phones
 # print(first_book)
 
 # one by filter
-wanted_book = collection_books.find_one({'author': 'Т.Г. Шевченко', 'price': 130})
-print(wanted_book)
+# wanted_book = collection_books.find_one({'author': 'Т.Г. Шевченко', 'price': 130})
+# print(wanted_book)
+
+# find many
+# books = collection_books.find()
+# # result = list(result)
+# # pprint(result)
+# for book in books:
+#     pprint(book)
+# print(55555555555555555)
+# for book in books:
+#     pprint(book)
+
+# query = {'author': 'Т.Г. Шевченко'}
+# query = {'price': 250}
+# query = {'price': {'$gt': 300}}
+# query = {'price': {'$gt': 300, '$lt': 500}}
+# query = {'price': {'$gte': 499, '$lte': 500.36}}
+# query = {
+#     'price': {'$gte': 499, '$lte': 500.36},
+#     'author': 'Т.Г. Шевченко',
+# }
+# query = {
+#     'title': {'$regex': 'I,*'},  # * -> any sequence of letters
+# }
+
+# query = {
+#     'content': {
+#         '$regex': 'десятеро',
+#         '$options': 'i',  # if i -> any register
+#     },
+# }
+# query = {
+#     'content': {
+#         '$regex': '^впер', # ^ -> field starts with expression
+#         '$options': 'i',  # if i -> any register
+#     },
+# }
+# query = {
+#     'content': {
+#         '$regex': '^в..рше ',  # . -> any single letter
+#         '$options': 'i',  # if i -> any register
+#     },
+# }
+
+# query = {
+#     'content': {
+#         '$regex': 'сві.у$',  # $ -> field ends with expression
+#         '$options': 'i',  # if i -> any register
+#     },
+# }
+# query = {
+#     'content': {
+#         '$not': {'$regex': 'сві.у$'}
+#     }
+# }
+# query = {}
+# query = {}
+#
+# books = collection_books.find(query).limit(5).sort('price', -1).skip(2)
+# for book in books:
+#     pprint(book)
+
+# number = int('10', 10)
+# print(number)
+# number = int('67ae429ea0c78ed781e21d9e', 16)  # 32087623706570306220426665374
+# print(number)
+# query = {'_id': ObjectId('67ae429ea0c78ed781e21d9e')}
+#
+# defined_book = collection_books.find_one(query)
+# pprint(defined_book)
+
+
+# UPDATE
+# use $set
+# query = {'price': 500.36}
+# new_data = {'$set': {'price': 510}}
+# updated = collection_books.update_one(query, new_data)
+# print(updated)
+
+# query = {'price': 510}
+# new_data = {'$set': {'price': 1200, 'newData': True}}
+# updated = collection_books.update_many(query, new_data)
+# print(updated)
+
+# multiplication
+# query = {}
+# new_data = {'$mul': {'price': 0.5}}
+# updated = collection_books.update_many(query, new_data)
+# print(updated)
+
+# increase
+# query = {}
+# new_data = {'$inc': {'price': -15}}
+# updated = collection_books.update_many(query, new_data)
+# print(updated)
+
+# together
+# query = {}
+# new_data = {'$inc': {'warranty': 10, 'cost': 3.5}, '$mul': {'price': 1.25}, '$set': {'brand': 'a-ba-ba-la-ma-ga'}}
+# updated = collection_books.update_many(query, new_data)
+# print(updated)
+
+#  DELETE field
+# query = {'_id': ObjectId('67ae429ea0c78ed781e21d9e')}
+# operation = {'$unset': {'warranty': 1}}
+# updated = collection_books.update_many(query, operation)
+# print(updated)
+
+#  DELETE document
+# query = {'_id': ObjectId('67ae429ea0c78ed781e21d9e')}
+# updated = collection_books.delete_many(query)
+# print(updated)
+
+collection_books.drop()
+client.drop_database(db)
+
